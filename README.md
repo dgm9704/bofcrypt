33269c057c39bbf0c9bf4c8405fed0c31efd9889
John Nordberg <john.nordberg@gmail.com>
2020-04-18 14:57:34 +0300

    update readme try to correct encryption add private/public keypair for testing add outputs for debugging

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ An attempt to create a reference implementation for encrypting report files for 
 
 **AT THE MOMENT THIS DOES NOT PRODUCE A CORRECT RESULT**
 
+- The file is in correct format and everything looks OK on the surface
+- The session key seems to be correctly generated and encrypted
+- There is some problem that prevents the actual file content from being decrypted, 
+  most likely something wrong with salting or the initialization vector
+
+
+
 Uses [openssl](https://www.openssl.org/) for encryption
 
 The public key was extracted from fin-fsa website
--- bofcrypt.sh
+++ bofcrypt.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 PUBLICKEY=$1
 INPUTFILE=$2
+
 #generate session key that is used to encrypt the file
-SESSIONKEY=$(openssl rand -hex 32)
+#SESSIONKEY=$(openssl rand -hex 32)
+
+#use dummy sessionkey for debugging purposes
+SESSIONKEY="0000000000000000000000000000000000000000000000000000000000000000"
+
+#initialization vector
+#IV="00112233445566778899aabbccddeeff"
+
+#use dummy initializationvector for debugging purposes
+IV="00000000000000000000000000000000"
+
 #encrypt the file using the generated session key
 #encrypt the session key using the recipients public key
-
 cat  << EOF
 <?xml version="1.0" encoding="utf-8"?>
 <ENCRYPTED_REPORT xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
   <TITLE>BOFCRYPTNXT</TITLE>
   <VERSION>2.0</VERSION>
   <SESSION_KEY>$(echo -n $SESSIONKEY | openssl rsautl -encrypt -inkey $PUBLICKEY -pubin -out >(base64 --wrap=0))</SESSION_KEY>
-  <OUT_BUFFER>$(openssl enc -aes-256-cbc -salt -pbkdf2 -base64 -A -pass pass:$SESSIONKEY -in $INPUTFILE)</OUT_BUFFER>
+  <OUT_BUFFER>$(openssl enc -aes-256-cbc -nosalt -pbkdf2 -base64 -A -K $SESSIONKEY -iv $IV -in $INPUTFILE)</OUT_BUFFER>
 </ENCRYPTED_REPORT>
 EOF

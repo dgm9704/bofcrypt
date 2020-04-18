2809d39dab1ba84b06b30c25958db46c3f84bc90
John Nordberg <john.nordberg@gmail.com>
2020-04-18 13:54:27 +0300

    use nicer syntax add example file add fin-fsa certificate from their website

--- bofcrypt.sh
+++ bofcrypt.sh
@@ -2,7 +2,7 @@
 PUBLICKEY=$1
 INPUTFILE=$2
 #generate session key that is used to encrypt the file
-SESSIONKEY=`openssl rand -hex 32`
+SESSIONKEY=$(openssl rand -hex 32)
 #encrypt the file using the generated session key
 #encrypt the session key using the recipients public key
 
@@ -11,7 +11,7 @@ cat  << EOF
 <ENCRYPTED_REPORT xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
   <TITLE>BOFCRYPTNXT</TITLE>
   <VERSION>2.0</VERSION>
-  <SESSION_KEY>`echo -n $SESSIONKEY | openssl rsautl -encrypt -inkey $PUBLICKEY -pubin -out >(base64 --wrap=0)`</SESSION_KEY>
-  <OUT_BUFFER>`openssl enc -aes-256-cbc -salt -pbkdf2 -base64 -A -pass pass:$SESSIONKEY -in $INPUTFILE`</OUT_BUFFER>
+  <SESSION_KEY>$(echo -n $SESSIONKEY | openssl rsautl -encrypt -inkey $PUBLICKEY -pubin -out >(base64 --wrap=0))</SESSION_KEY>
+  <OUT_BUFFER>$(openssl enc -aes-256-cbc -salt -pbkdf2 -base64 -A -pass pass:$SESSIONKEY -in $INPUTFILE)</OUT_BUFFER>
 </ENCRYPTED_REPORT>
 EOF
